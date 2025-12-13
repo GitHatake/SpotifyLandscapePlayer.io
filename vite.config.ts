4556692469565cb9ca9a41079105e8c6a52381f9
HATAKEYAMA Yuki <a24117@numazu.kosen-ac.jp>
2025-12-13 21:03:25 +0900

    feat: init project with vite, tailwind, pwa

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/SpotifyLandscapePlayer.io/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'Spotify Landscape Player',
+        short_name: 'SpotifyPlayer',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#121212',
+        theme_color: '#121212',
+        description: 'Immersive Spotify Landscape Player',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
